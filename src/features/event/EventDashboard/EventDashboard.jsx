d8fa12376222552d60829874e10e872a803b648a
justasces <justascesnauskas7@gmail.com>
2018-07-05 14:09:54 +0300

    add update, delete , read

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const eventsDashboard = [
   {
     id: '1',
     title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
+    date: '2018-03-27',
     category: 'culture',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -32,7 +32,7 @@ const eventsDashboard = [
   {
     id: '2',
     title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
+    date: '2018-03-28',
     category: 'drinks',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -57,39 +57,67 @@ const eventsDashboard = [
 
 
 class EventDashboard extends Component {
-
   state = {
     events: eventsDashboard,
-    isOpen: false
+    isOpen: false,
+    selectedEvent: null
   };
 
   handleFormOpen = () => {
     this.setState({
+      selectedEvent: null,
       isOpen: true
     });
-  }
+  };
 
   handleFormCancel = () => {
     this.setState({
       isOpen: false
     });
-  }
+  };
 
-  handleCreateEvent = (newEvent) => {
+  handleOpenEvent = eventToOpen => () => {
+    this.setState({
+      selectedEvent: eventToOpen,
+      isOpen: true
+    });
+  };
+
+  handleUpdateEvent = updateEvent => {
+    this.setState({
+      events: this.state.events.map(event => {
+        if (event.id === updateEvent.id) {
+          return Object.assign({}, updateEvent);
+        } else {
+          return event;
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
+    });
+  };
+
+  handleCreateEvent = newEvent => {
     newEvent.id = cuid();
     newEvent.hostPhotoURL = "/assets/user.png";
     const updatedEvents = [...this.state.events, newEvent];
     this.setState({ events: updatedEvents, isOpen: false });
+  };
+
+  handleDeleteEvent = (eventId) => () => {
+    const updatedEvents = this.state.events.filter(e => e.id !== eventId);
+    this.setState({ events: updatedEvents });  
   }
 
   render() {
+    const { selectedEvent } = this.state;
     return <Grid>
         <Grid.Column width={10}>
-          <EventList events={this.state.events} />
+          <EventList onEventOpen={this.handleOpenEvent} deleteEvent={this.handleDeleteEvent} events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button onClick={this.handleFormOpen} positive content="Create Event" />
-          {this.state.isOpen && <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleFormCancel} />} {/* && - jei true EventForm matosi jei false ne*/}
+          {this.state.isOpen && <EventForm selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleFormCancel} updatedEvents={this.handleUpdateEvent} />} {/* && - jei true EventForm matosi jei false ne*/}
         </Grid.Column>
       </Grid>;
   }
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 
+const emptyEvent = {
+    title: '',
+    date: '',
+    city: '',
+    venue: '',
+    hostedBy: ''
+}
+
 class EventForm extends Component {
+  
     state ={
-        event : {
-            title: '',
-            date: '',
-            city: '',
-            venue: '',
-            hostedBy: ''
+        event: emptyEvent
+    }
+
+    componentDidMount() {
+        if(this.props.selectedEvent !== null) {
+            this.setState({
+                event: this.props.selectedEvent
+            })
+        }    
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.selectedEvent !== this.props.selectedEvent) {
+            this.setState({ event: nextProps.selectedEvent || emptyEvent });
         }
     }
+
     onFormSubmit = evt => {
         evt.preventDefault();
-        console.log(this.props);
-        this.props.createEvent(this.state.event);
+        if(this.state.event.id) {
+            this.props.updatedEvents(this.state.event);
+        } else {
+            this.props.createEvent(this.state.event);
+        }
     };
 
     onInputChange = (evt) => {
@@ -36,19 +57,19 @@ class EventForm extends Component {
             </Form.Field>
             <Form.Field>
                 <label>Event Date</label>
-                <input name="date" onChange={this.onInputChange} type="date" placeholder="Event Date" />
+                <input name="date" value={event.date} onChange={this.onInputChange} type="date" placeholder="Event Date" />
             </Form.Field>
             <Form.Field>
                 <label>City</label>
-                <input name="city" onChange={this.onInputChange} placeholder="City event is taking place" />
+                <input name="city" value={event.city} onChange={this.onInputChange} placeholder="City event is taking place" />
             </Form.Field>
             <Form.Field>
                 <label>Venue</label>
-                <input name="venue" onChange={this.onInputChange} placeholder="Enter the Venue of the event" />
+                <input name="venue" value={event.venue} onChange={this.onInputChange} placeholder="Enter the Venue of the event" />
             </Form.Field>
             <Form.Field>
                 <label>Hosted By</label>
-                <input name="hostedBy" onChange={this.onInputChange} placeholder="Enter the name of person hosting" />
+                <input name="hostedBy" value={event.hostedBy} onChange={this.onInputChange} placeholder="Enter the name of person hosting" />
             </Form.Field>
             <Button positive type="submit">
                 Submit
