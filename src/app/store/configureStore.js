491c4ee7f96b0be9617802b78776289a042f71d0
justasces <justascesnauskas7@gmail.com>
2018-07-10 12:03:01 +0300

    added redux

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { rootReducer } from "../reducers/rootReducer";
+
+export const configureStore = (preloadeState) => {
+    const middlewares = [];
+    const middlewareEnchancer = applyMiddleware(...middlewares);
+
+    const storeEnchancers = [middlewareEnchancer];
+
+    const composedEnchacers = composeWithDevTools(...storeEnchancers);
+
+    const store = createStore(
+        rootReducer,
+        preloadeState,
+        composedEnchacers
+    );
+
+    if(process.env.NODE_ENV != "production") {
+        if(module.hot) {
+            module.hot.accept('../reducers/rootReducer', () => {
+                const newRootReducer = require('../reducers/rootReducer').default;
+                store.replaceReducer(newRootReducer);
+            })
+        }
+    }
+    return store;
+}
