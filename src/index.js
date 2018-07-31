3886eeb7b7eb94986d21e33cdcb490e4aa1ea417
justasces <justascesnauskas7@gmail.com>
2018-07-31 22:02:57 +0300

    Added thunk , mocked api ,async

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import "./index.css";
 import App from "./app/layout/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { configureStore } from "./app/store/configureStore";
-import ScrollToTop  from './app/common/util/ScrollToTop'
+import ScrollToTop  from './app/common/util/ScrollToTop';
+import {loadEvents} from './features/event/eventActions'
 
 const store = configureStore();
+store.dispatch(loadEvents());
 
 const rootEl = document.getElementById("root");
 let render = () => {
